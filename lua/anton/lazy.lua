24f4f32ce4c6f7ec41c20331adecaeb394d75c55
uncletonio <antonzhuikov@gmail.com>
2025-03-04 22:19:59 +0000

    finish setup

--- lua/anton/lazy.lua
+++ lua/anton/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("anton.plugins", {
+require("lazy").setup({ { import = "anton.plugins" }, { import = "anton.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
